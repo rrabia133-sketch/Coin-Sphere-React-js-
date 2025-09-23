143fe1d526bbb5ec4e79e4b0d672d90ff1037f3a
rrabia133-sketch <rrabia133@gmail.com>
2025-09-23 15:44:55 +0800

    done

--- Coin-Sphare/src/Components/SideNav.jsx
+++ Coin-Sphare/src/Components/SideNav.jsx
@@ -41,7 +41,7 @@ const Sidenav = () => {
     >
       <Box>
         <Heading textAlign="center" fontSize="20px" as="h1" pt="3.5rem">
-          @DOSOMECODING
+          Coin Sphare
         </Heading>
         <Box mt="6" mx="3">
           {navLinks.map((nav) => (
